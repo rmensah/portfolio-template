271fd93e5cae2cb436d9f619409749fc8776d8d0
RM <rm@RMs-MacBook-Pro.fios-router.home>
2016-04-19 13:08:16 -0400

    re-organized folder structure

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.get('/', function(req, res){
-	res.sendFile(path.join(__dirname + '/MaterialX - HTML v1.1/materialx/index.html'))});
+// var middleware = require('express-middleware');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', function(req, res){
+	res.sendFile(process.cwd() + '/public/index.html')});
 
 
 var port = 3000;
