ef3126d7b40a66c3b124119d0e1d2e75d612a62b
RM <rm@RMs-MacBook-Pro.fios-router.home>
2016-04-25 12:25:36 -0400

    updated map location code and corresponding index.html text

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -546,8 +546,8 @@ $(document).ready(function($){
 	if ( $mapWrapper.length > 0 ) {
 		var map = new GMaps({
 			div: '#map',
-			lat : 23.79473005386213,
-			lng : 90.41430473327637,
+			lat : 40.488437,
+			lng : -74.4759872,
 			scrollwheel: false,
 			draggable: draggableOp,
 			zoom: 16,
@@ -556,11 +556,11 @@ $(document).ready(function($){
 		});
 
 		map.addMarker({
-			lat : 23.79473005386213,
-			lng : 90.41430473327637,
+			lat : 40.488437,
+			lng : -74.4759872,
 			icon: 'images/marker-icon.png',
 			infoWindow: {
-				content: '<p>BD InfoSys Ltd, Dhaka, Bangladesh</p>'
+				content: '<p>New Brunswick, New Jersey, USA</p>'
 			}
 		});
 	}
